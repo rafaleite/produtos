85324011ca19e80ffef4c8ec1181166d169ade5f
Rafael Leite <rafael@rleite.com.br>
2017-07-07 07:17:52 -0300

    First Commit

---
+++ src/Produtos.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Produtos extends Component {
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-md-2'>
+                    <h3>Categorias</h3>
+                    Links
+                </div>
+                <div className='col-md-10'>
+                    <h1>Produtos</h1>
+                </div>
+            </div>
+        )
+    }
+}
